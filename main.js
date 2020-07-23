1ea3865bddbee8697361836d3b08330b268dae2c
Sir Rodriguez <dr.raw.phd@gmail.com>
2020-07-23 17:07:23 -0400

    first commit

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ class Traveler {
     }
     hunt () {return this.food += 2}
     eat () {
-        if (this.food <= 0) {this.food = 0; this.isHealthy = false}
-        else return this.food -= 1}
+        if (this.food === 0) {this.isHealthy = false} 
+        else {return this.food -= 1}
+    }
 }
 
 class Wagon {
@@ -25,9 +26,13 @@ class Wagon {
         }
     }
     shouldQuarantine () {
-        for (let counter = 0; counter < this.passengers.length; counter += 1) {
-        if ((this.passengers[counter]).isHealthy === false) {return true}
+        let healthStatusArray = []
+        for (let counter = 0; counter <this.passengers.length; counter += 1) {
+            healthStatusArray.push(this.passengers[counter].isHealthy)
         }
+        return healthStatusArray.some(function (value, index, array) {
+            if (value === false) {return true} else {return false}
+        })
     }
     totalFood () {
         let foodArray = []
@@ -37,5 +42,35 @@ class Wagon {
         return foodArray.reduce(function (travelerX, travelerY) {
             return travelerX + travelerY
         })
+    }
+}
+
+class Doctor extends Traveler {
+    constructor (name, food) {
+        super(name, food)
+    }
+    heal (traveler) {traveler.isHealthy = true}
+}
+
+class Hunter extends Traveler {
+    constructor(name, food) {
+        super (name, food)
+        this.food = 2
+    }
+    hunt () {this.food += 5}
+
+    eat () {
+        this.food -= 2
+        if (this.food < 2) {
+            this.food = 0
+            this.isHealthy = false
+        }
+    }
+
+    giveFood (traveler, numOfFoodUnits) {
+        if (numOfFoodUnits < this.food) {
+            traveler.food += numOfFoodUnits
+            this.food -= numOfFoodUnits
+        }
     }
 }
